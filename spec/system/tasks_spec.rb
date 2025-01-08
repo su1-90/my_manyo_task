e5bf92ee0a83e93f92f58801dc87bf37524834e4
fujiisui <sui_face@yahoo.co.jp>
2025-01-08 09:07:38 +0900

    system spec 1~5

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -14,18 +14,25 @@ RSpec.describe 'タスク管理機能', type: :system do
   end
 
   describe '一覧表示機能' do
+
+    # let! を使用　テストデータを用意
+    let!(:task1) { FactoryBot.create(:task, title: 'first_task', created_at: '2022-02-18') }
+    let!(:task2) { FactoryBot.create(:task, title: 'second_task', created_at: '2022-02-17') }
+    let!(:task3) { FactoryBot.create(:task, title: 'third_task', created_at: '2022-02-16') }
+
+    # beforeブロックを使用 共通のセットアップ
+    before do
+      visit tasks_path
+    end
+
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が作成日時の降順で表示される' do
-        older_task = FactoryBot.create(:task, title: '古いタスク', created_at: 1.day.ago)
-        newer_task = FactoryBot.create(:task, title: '新しいタスク', created_at: 1.hour.ago)
-        visit tasks_path
         task_list = all('.task-title').map(&:text)
-        expect(task_list).to eq ['新しいタスク', '古いタスク']
+        expect(task_list).to eq ['first_task', 'second_task', 'third_task']
       end
     end
     context '新たにタスクを作成した場合' do
       it '新しいタスクが一番上に表示される' do
-        FactoryBot.create(:task, title: '既存タスク', created_at: 1.day.ago)
         visit new_task_path
         fill_in 'task_title', with: '新しいタスク'
         fill_in 'task_content', with: '新しいタスクの内容'
