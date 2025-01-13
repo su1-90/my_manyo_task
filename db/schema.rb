9f44a9ffc773019f0e9c8765b6aec428999c65aa
fujiisui <sui_face@yahoo.co.jp>
2025-01-13 18:19:44 +0900

    11.検索インデックスを貼る

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_01_12_032602) do
+ActiveRecord::Schema.define(version: 2025_01_13_091839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2025_01_12_032602) do
     t.date "deadline_on", null: false
     t.integer "priority", null: false
     t.integer "status", null: false
+    t.index ["status"], name: "index_tasks_on_status"
   end
 
 end
